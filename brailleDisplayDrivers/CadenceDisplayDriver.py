f17bd1f1e3aacb7bec1741ca6b8801a5997abfe1
yyon <linkkirby7@gmail.com>
2024-08-29 15:18:55 -0400

    - keep aspect ratio - fixed some bugs - groundwork for panning/zooming

--- brailleDisplayDrivers/CadenceDisplayDriver.py
+++ brailleDisplayDrivers/CadenceDisplayDriver.py
@@ -7,6 +7,68 @@ from NVDAObjects import NVDAObject
 from screenBitmap import ScreenBitmap
 import math
 import threading
+import ctypes
+import winGDI
+
+user32 = ctypes.windll.user32
+gdi32 = ctypes.windll.gdi32
+
+class ScreenBitmapResized(ScreenBitmap):
+	def __init__(self, width, height):
+		super().__init__(width, height)
+
+	def captureImage(self, x, y, w, h, x2, y2, w2, h2):
+		"""
+		Captures the part of the screen starting at x,y and extends by w (width) and h (height), and stretches/shrinks it to fit in to the object's bitmap size.
+		"""
+		tempDC = gdi32.CreateCompatibleDC(self._screenDC)
+		tempMemBitmap = gdi32.CreateCompatibleBitmap(self._screenDC, w, h)
+		tempOldBitmap = gdi32.SelectObject(tempDC, tempMemBitmap)
+
+		# Copy the requested content from the screen in to our memory device context, stretching/shrinking its size to fit.
+		gdi32.StretchBlt(
+			tempDC,
+			0,
+			0,
+			w,
+			h,
+			self._screenDC,
+			x,
+			y,
+			w,
+			h,
+			winGDI.SRCCOPY,
+		)
+		gdi32.StretchBlt(
+			self._memDC,
+			0,
+			0,
+			self.width,
+			self.height,
+			tempDC,
+			x2,
+			y2,
+			w2,
+			h2,
+			winGDI.SRCCOPY,
+		)
+		# Fetch the pixels from our memory bitmap and store them in a buffer to be returned
+		buffer = (winGDI.RGBQUAD * self.width * self.height)()
+		gdi32.GetDIBits(
+			self._memDC,
+			self._memBitmap,
+			0,
+			self.height,
+			buffer,
+			ctypes.byref(self._bmInfo),
+			winGDI.DIB_RGB_COLORS,
+		)
+
+		gdi32.SelectObject(tempDC, tempOldBitmap)
+		gdi32.DeleteObject(tempMemBitmap)
+		gdi32.DeleteDC(tempDC)
+
+		return buffer
 
 def isSupportEnabled() -> bool:
 	return bdDetect.driverIsEnabledForAutoDetection(CadenceDisplayDriver.name)
@@ -41,16 +103,21 @@ class RunInterval(threading.Thread):
 	callback = None
 
 	def __init__(self, callback, interval = 1):
-		threading.Thread.__init__(self)
+		super().__init__()
 		self.callback = callback
 		self.interval = interval
+		self.daemon = True
 	
 	def cancel(self):
 		self.stopFlag.set()
 
 	def run(self):
 		while not self.stopFlag.wait(self.interval):
-			self.callback()
+			try:
+				self.callback()
+			except Exception as e:
+				log.error(f"{e}")
+				pass
 
 class CadenceDisplayDriver(HidBrailleDriver):
 	name = "CadenceDisplayDriver"
@@ -60,6 +127,13 @@ class CadenceDisplayDriver(HidBrailleDriver):
 	displayingImage = False
 	lastDisplayedNonImage = None
 	imageTimer = None
+
+	centerX = 0.5
+	centerY = 0.5
+	zoomX = 1
+	zoomY = 1
+	lastFitWidth = -1
+	lastFitHeight = -1
 
 	@classmethod
 	def registerAutomaticDetection(cls, driverRegistrar: bdDetect.DriverRegistrar):
@@ -79,7 +153,7 @@ class CadenceDisplayDriver(HidBrailleDriver):
 	def doToggleImage(self):
 		self.displayingImage = not self.displayingImage
 		if self.displayingImage:
-			self.displayImage()
+			self.displayImage(True)
 			if self.imageTimer is None:
 				self.imageTimer = RunInterval(self.displayImage, 0.5)
 				self.imageTimer.start()
@@ -89,18 +163,36 @@ class CadenceDisplayDriver(HidBrailleDriver):
 				self.imageTimer.cancel()
 				self.imageTimer = None
 	
-	def displayImage(self):
+	def displayImage(self, resetView = False):
 		obj = api.getNavigatorObject()
+		if obj is None:
+			obj = api.getFocusObject()
+			if obj is None:
+				log.error("no navigator object")
+				return
 		location = obj.location
 		if not location:
 			log.error("no location for object when displaying image")
 			return
 		(left, top, width, height) = location
-		log.info(f"######## sscreenshot {location} {left} {top} {width} {height}")
-		screenWidth = self.numCols * 2
-		screenHeight = self.numRows * 4
-		bitmapHolder = ScreenBitmap(screenWidth, screenHeight)
-		bitmapBuffer = bitmapHolder.captureImage(left, top, width, height)
+		log.info(f"######## screenshot {left} {top} {width} {height}")
+		if width <= 0 or height <= 0:
+			log.error("invalid object location")
+			return
+		if resetView or self.lastFitWidth != width or self.lastFitHeight != height:
+			self.reset(width, height)
+		screenWidth = self.getDisplayWidth()
+		screenHeight = self.getDisplayHeight()
+
+		topLeftX = self.screenXToVirtual(0, self.getDisplayWidth()) * width
+		bottomRightY = self.screenYToVirtual(0, self.getDisplayHeight()) * height
+		bottomRightX = self.screenXToVirtual(self.getDisplayWidth(), self.getDisplayWidth()) * width
+		topLeftY = self.screenYToVirtual(self.getDisplayHeight(), self.getDisplayHeight()) * height
+
+		log.info(f"{topLeftX} {topLeftY} {bottomRightX} {bottomRightY}")
+
+		bitmapHolder = ScreenBitmapResized(screenWidth, screenHeight)
+		bitmapBuffer = bitmapHolder.captureImage(left, top, width, height, round(topLeftX), round(topLeftY), round(bottomRightX - topLeftX), round(bottomRightY - topLeftY))
 		testImage = [bitmapToCell(bitmapBuffer, i, self.numCols, self.numRows) for i in range(self.numCells)]
 		self.display(testImage, True)
 	
@@ -119,5 +211,34 @@ class CadenceDisplayDriver(HidBrailleDriver):
 		if self.imageTimer is not None:
 			self.imageTimer.cancel()
 			self.imageTimer = None
+	
+	def getDisplayWidth(self):
+		return self.numCols * 2
+	def getDisplayHeight(self):
+		return self.numRows * 4
+	def getFitZoom(self, toDrawWidth, toDrawHeight):
+		# calculate zoom to fit image in screen while keeping aspect ratio
+		if (toDrawWidth / toDrawHeight > self.getDisplayWidth() / self.getDisplayHeight()):
+			# image wider than screen
+			return [2, toDrawHeight / (toDrawWidth * (self.getDisplayHeight() / self.getDisplayWidth())) * 2]
+		else:
+			return [toDrawWidth / (toDrawHeight * (self.getDisplayWidth() / self.getDisplayHeight())) * 2, 2]
+	def reset(self, toDrawWidth, toDrawHeight):
+		self.centerX = 0.5
+		self.centerY = 0.5
+		fitZoom = self.getFitZoom(toDrawWidth, toDrawHeight)
+		self.zoomX = fitZoom[0]
+		self.zoomY = fitZoom[1]
+		self.lastFitWidth = toDrawWidth
+		self.lastFitHeight = toDrawHeight
+	def virtualXToScreen(self, actualX, graphWidth):
+		return (actualX - self.centerX) * self.zoomX * ((graphWidth) / 2) + (graphWidth) / 2
+	def virtualYToScreen(self, actualY, graphHeight):
+		return graphHeight - ((actualY - self.centerY) * self.zoomY * ((graphHeight) / 2) + (graphHeight) / 2)
+	def screenXToVirtual(self, graphX, graphWidth):
+		return ((graphX) - ((graphWidth) / 2)) / ((graphWidth) / 2) / self.zoomX + self.centerX
+	def screenYToVirtual(self, graphY, graphHeight):
+		return ((graphHeight - graphY) - ((graphHeight) / 2)) / ((graphHeight) / 2) / self.zoomY + self.centerY
+
 
 BrailleDisplayDriver = CadenceDisplayDriver
